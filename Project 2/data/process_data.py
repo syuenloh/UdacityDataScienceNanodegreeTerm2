4674329d897edaf89b5cbd9fb9e244ac6dfb9866
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-06 22:05:52 +0800

    fix: fix save_data function to be able to take in database_filename input

--- Project 2/data/process_data.py
+++ Project 2/data/process_data.py
@@ -29,7 +29,7 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///database_filename.db')
+    engine = create_engine('sqlite:///'+database_filename+'.db')
     df.to_sql(database_filename, engine, index=False)  
 
 

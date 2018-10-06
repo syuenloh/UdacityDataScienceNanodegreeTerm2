65710159b2930dcd5e1a114b0b1ee0ec253d8b7e
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-06 22:05:01 +0800

    feat: add code for building and training model

--- Project 2/models/train_classifier.py
+++ Project 2/models/train_classifier.py
@@ -1,25 +1,73 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+nltk.download(['punkt', 'wordnet'])
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///'+database_filepath+'.db')
+    df = pd.read_sql_table(database_filepath,engine)
+    X = df.loc[:,'message']
+    Y = df.iloc[:,-36:]
+    category_names=list(Y.columns)
+    return X,Y,category_names
+    
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())) 
+         ])
+    
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for a,b in enumerate(category_names):
+        print("For column {}:".format(b))
+        print(classification_report(Y_test[b], y_pred[:,a]))
+    print("\nBest Parameters:", cv.best_params_)
 
 
 def save_model(model, model_filepath):
-    pass
-
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
 
 def main():
     if len(sys.argv) == 3:

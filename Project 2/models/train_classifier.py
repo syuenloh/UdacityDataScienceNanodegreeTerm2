eb7a749daf6c81b44f555dd22c4363bb8cefbf63
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-09 17:17:50 +0800

    fix: commented out grid search
    Commented out grid search portion of code since It takes too long to train the model to run the app

--- Project 2/models/train_classifier.py
+++ Project 2/models/train_classifier.py
@@ -42,19 +42,20 @@ def build_model():
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier())) 
          ])
-    
-    parameters = {
-        'vect__ngram_range': ((1, 1), (1, 2)),
-        'vect__max_df': (0.5, 0.75, 1.0),
-        'vect__max_features': (None, 5000, 10000),
-        'tfidf__use_idf': (True, False),
-        'clf__estimator__n_estimators': [50, 100, 200],
-        'clf__estimator__min_samples_split': [2, 3, 4],
-    }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+# Note: commented out because it takes too long to train the model
+#     parameters = {
+#         'vect__ngram_range': ((1, 1), (1, 2)),
+#         'vect__max_df': (0.5, 0.75, 1.0),
+#         'vect__max_features': (None, 5000, 10000),
+#         'tfidf__use_idf': (True, False),
+#         'clf__estimator__n_estimators': [50, 100, 200],
+#         'clf__estimator__min_samples_split': [2, 3, 4],
+#     }
 
-    return cv
+#     cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -62,7 +63,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
     for a,b in enumerate(category_names):
         print("For column {}:".format(b))
         print(classification_report(Y_test[b], y_pred[:,a]))
-    print("\nBest Parameters:", cv.best_params_)
+#     print("\nBest Parameters:", cv.best_params_)
 
 
 def save_model(model, model_filepath):
